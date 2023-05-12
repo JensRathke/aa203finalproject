9778cbdfec1524d93d05f54cce24c6e1fac49621
Jens Rathke <jens.rathke@gmail.com>
2023-05-13 01:39:03 +0200

    Additions to qaudcopter and simulation

--- Code/quadcopter.py
+++ Code/quadcopter.py
@@ -2,27 +2,99 @@
 
 import numpy as np
 
-def quadcopter_dynamics(s, u):
-    x, vx, y, vy, phi, omega = s
-    T1, T2 = u
-    m = 450 # kg
-    g = 9.81 # m/s²
-    l = 4.6 # m (length of a rotor arm)
-    r = 1.5 # m (radius of the cabin)
-    Ixx = (2 * m * (r^2) / 5) + 2 * m * (l^2) 
-    Iyy = (2 * m * (r^2) / 5) + 2 * m * (l^2)
-    Izz = (2 * m * (r^2) / 5) + 4 * m * (l^2)
+class QuadcopterPlanar:
+    """ Planar Quadcopter """
+    def __init__(self, mass = 450, len_rotor_arm = 4.6, cabin_radius = 1.5):
+        """
+        Functionality
+            Quadcopter class
+        
+        Parameters
+            mass: mass of the quadcopter (default 450 kg)
+            len_rotor_arm: length of a rotor arm (default 4.6 m)
+            cabin_radius: radius of the cabin (default 1.5 m)
+        """
+        self.m = mass
+        self.l = len_rotor_arm
+        self.r = cabin_radius
+        self.g = 9.81 # m/s²
 
-    ds = np.array([
-        vx,
-        (-(T1 + T2) * np.sin(phi)) / m,
-        vy,
-        ((T1 + T2) * np.cos(phi)) / m - g,
-        omega,
-        ((T2 - T1) * l) / Izz
-    ])
+    def quadcopter_dynamics(self, s, u):
+        """
+        Functionality
+            Quadcopter dynamics
+        
+        Parameters
+            s: state (x, dx, y, dy, phi, omega)
+            u: control input (t1, t2)
 
-    return ds
+        Returns
+            derivative of the state with respect to time
+        """
+        x, dx, y, dy, phi, omega = s
+        t1, t2 = u
+
+        Ixx = (2 * self.m * (self.r^2) / 5) + 2 * self.m * (self.l^2) 
+        Iyy = (2 * self.m * (self.r^2) / 5) + 2 * self.m * (self.l^2)
+        Izz = (2 * self.m * (self.r^2) / 5) + 4 * self.m * (self.l^2)
+
+        ds = np.array([
+            dx,
+            (-(t1 + t2) * np.sin(phi)) / self.m,
+            dy,
+            ((t1 + t2) * np.cos(phi)) / self.m - self.g,
+            omega,
+            ((t2 - t1) * self.l) / Izz
+        ])
+
+        return ds
+    
+class QuadcopterCubic:
+    """ Cubic Quadcopter """
+    def __init__(self, mass = 450, len_rotor_arm = 4.6, cabin_radius = 1.5):
+        """
+        Functionality
+            Quadcopter class
+        
+        Parameters
+            mass: mass of the quadcopter (default 450 kg)
+            len_rotor_arm: length of a rotor arm (default 4.6 m)
+            cabin_radius: radius of the cabin (default 1.5 m)
+        """
+        self.m = mass
+        self.l = len_rotor_arm
+        self.r = cabin_radius
+        self.g = 9.81 # m/s²
+
+    def quadcopter_dynamics(self, s, u):
+        """
+        Functionality
+            Quadcopter dynamics
+        
+        Parameters
+            s: state (x, dx, y, dy, phi, omega)
+            u: control input (t1, t2)
+
+        Returns
+            derivative of the state with respect to time
+        """
+        x, dx, y, dy, phi, omega = s
+        t1, t2 = u
+
+        Ixx = (2 * self.m * (self.r^2) / 5) + 2 * self.m * (self.l^2) 
+        Iyy = (2 * self.m * (self.r^2) / 5) + 2 * self.m * (self.l^2)
+        Izz = (2 * self.m * (self.r^2) / 5) + 4 * self.m * (self.l^2)
+
+        ds = np.array([
+            dx,
+            (-(t1 + t2) * np.sin(phi)) / self.m,
+            dy,
+            ((t1 + t2) * np.cos(phi)) / self.m - self.g,
+            omega,
+            ((t2 - t1) * self.l) / Izz
+        ])
+
+        return ds
 
 if __name__ == "__main__":
     pass
--- Code/simulation.py
+++ Code/simulation.py
@@ -1,7 +1,26 @@
 #!/usr/bin/env python3
 
+class SimulationPlanar:
+    """ Simulation for a planar Quadcopter """
+    def __init__(self):
+        """
+        Functionality
+            Initialisation
+        
+        Parameters
+            
+        """
 
-
+class SimulationCubic:
+    """ Simulation for a planar Quadcopter """
+    def __init__(self):
+        """
+        Functionality
+            Initialisation
+        
+        Parameters
+            
+        """
 
 if __name__ == "__main__":
     pass
