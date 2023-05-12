64ff4f5eb34b991b101793054e533d018375d814
Jens Rathke <jens.rathke@gmail.com>
2023-05-12 23:36:41 +0200

    Minor change in main

--- Code/main.py
+++ Code/main.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 import numpy as np
-<<<<<<< HEAD
+
+import matplotlib.pyplot as plt
+import jax
 
 from simulation import *
 from quadcopter import *
@@ -9,10 +11,6 @@ from spacecraft import *
 from controller import *
 from plotting import *
 from animation import *
-=======
-import matplotlib.pyplot as plt
-import jax
->>>>>>> deac609fc4082dd351594a86266107821397188b
 
 if __name__ == '__main__':
 
