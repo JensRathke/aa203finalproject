8314f1c1d417b4b1ede8ca3ed75499b9385391a6
JensRathke <jens.rathke@gmail.com>
2023-05-14 22:26:21 +0200

    minor changes

--- Code/main.py
+++ Code/main.py
@@ -9,6 +9,20 @@ from controller import *
 from plotting import *
 from animation import *
 
+"""
+******
+Simulation of different controllers to land a quadcopter on a moving landing pad.
+******
+
+States
+======
+state of the quadcopter in 2D: (x, y, dx, dy, phi, omega)
+state of the quadcopter in 3D: (x, y, z, dx, dy, dz, phi, theta, psi, dphi, dtheta, dpsi)
+
+state of the landing pad in 2D: (x, y, 0, 0, phi, 0)
+state of the landing pad in 3D: (x, y, z, 0, 0, 0, phi, theta, psi, 0, 0, 0)
+"""
+
 if __name__ == '__main__':
 
     quadcopter = QuadcopterPlanar()
